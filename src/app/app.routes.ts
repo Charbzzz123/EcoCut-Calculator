5dd024512ee221a70d3f5e8317272b3256340d80
Charbzzz123 <Charbzzz123@gmail.com>
2026-01-29 16:10:13 -0500

    feat: replace starter shell with home route wrapper

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeShellComponent } from './home/home-shell.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  {
+    path: 'home',
+    component: HomeShellComponent,
+    title: 'EcoCut | Home',
+  },
+  { path: '**', redirectTo: 'home' },
+];
